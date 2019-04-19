937997d159130a4a1167ce0429111ebfe518f2f1
maks54rus <49484189+maks54rus@users.noreply.github.com>
2019-04-19 23:16:25 +0900

    Update 65.sh

--- 65.sh
+++ 65.sh
@@ -1,3 +1,3 @@
 pkill konina
 sleep 7
-konina -o 95.216.214.218:5755 -u 4AvKpmkjuJUVxTYTRR4TwzFniizuQ3EuzVSvgtjDiTfuPDFEGEMd92q2CSKCiBvASLbWeSvC16Wn7PejdxEzszbdVZWK1Qj --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=44 --av=1 --variant -1
+konina -o 95.216.214.218:5755 -u 43QGgipcHvNLBX3nunZLwVQpF6VbobmGcQKzXzQ5xMfJgzfRBzfXcJHX1tUHcKPm9bcjubrzKqTm69JbQSL4B3f6E3mNCbU --pass=12:r199719971997@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=44 --av=1 --variant -1
